f11a3e4ac0e12464b949c4e7c8431ac3bf269224
Academy-of-Code <dillioncat77@gmail.com>
2023-03-20 16:59:00 -0400

    Added stuffz

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.4'
+VERSION = '0.4.2'
 DESCRIPTION = 'A simple but powerful GUI python package for overlays. Forked from EthanEdits/overlayGUI'
 LONG_DESCRIPTION = 'A python package that uses Pygame to add an overlay to Windows'
 
@@ -21,10 +21,10 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['pygame', 'pywin32', 'time'],
+    install_requires=['pygame', 'pywin32'],
     keywords=['python', 'overlay', 'pygame', 'powerful', 'easy', 'quick'],
     classifiers=[
-        "Development Status :: Complete",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Microsoft :: Windows",
